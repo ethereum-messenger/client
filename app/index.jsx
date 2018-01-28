6be71d5ac6aed87c11ef0288b68fa0033d6874b6
ryan-chomistek <ryan.chomistek@tamu.edu>
2018-01-28 10:25:36 -0600

    ZXC

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Application from './components/Application.jsx';
+import "normalize.css/normalize.css";
 import "../styles/index.scss";
 //import './actions'
 
